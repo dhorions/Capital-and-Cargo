594245bbe06cedb5c257dd558e514f62dfb3c3ea
dhorions <dries@quodlibet.be>
2024-04-22 17:23:58 +0200

    fix crash when clicking sell when the warehouse is empty

--- Program.cs
+++ Program.cs
@@ -1178,6 +1178,11 @@ class Program
     }
     private static void sellDialog()
     {
+        if(cityGoodsListView.SelectedRow < 0)
+        {
+            return;
+        }
+
         //pause();//pause game loop when in dialog
         String CargoType = (String)cityGoodsListView.Table.Rows[cityGoodsListView.SelectedRow]["CargoType"];
         String city = (String)citiesListView.Table.Rows[citiesListView.SelectedRow]["City"];
